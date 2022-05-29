8e3d30ec268d542b9d3bb4e623e8a9933d7f2b25
MintSW <mintnimal@gmail.com>
2022-05-30 02:30:24 +0700

    First Commit

--- AboutMe/ContentView.swift
+++ AboutMe/ContentView.swift
@@ -9,8 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+            Text("Home")
+                .tabItem {
+                    Label("Home", systemImage: "person")
+                }
+                .tag(1)
+            Text("Story")
+                .tabItem {
+                    Label("Story", systemImage: "book")
+                }
+                .tag(2)
+            Text("Favorite")
+                .tabItem {
+                    Label("Favorite", systemImage: "star")
+                }
+                .tag(3)
+            Text("Fun Facts")
+                .tabItem {
+                    Label("Fun Facts", systemImage: "hand.thumbsup")
+                }
+                .tag(4)
+        }
     }
 }
 
